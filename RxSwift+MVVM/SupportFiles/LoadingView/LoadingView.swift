d4dabeab80a0c4ee2d0e414029f3f25c5867b972
Mustafa GUNES <gunes149@gmail.com>
2019-02-12 13:17:09 +0300

    LoadingView set

--- RxSwift+MVVM/SupportFiles/LoadingView/LoadingView.swift
+++ RxSwift+MVVM/SupportFiles/LoadingView/LoadingView.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class LoadingView: UIViewController {
+class LoadingView: UIView {
     
     // MARK : - Outlets
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet var containerView: UIView!
+    @IBOutlet weak var animateView: AnimateLoadingView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    public var loadingViewMessage: String! {
+        didSet {
+            messageLabel.text = loadingViewMessage
+        }
+    }
+    
+    public func startAnimating() {
+        if animateView.isAnimating { return }
+        animateView.startAnimating()
+    }
+    
+    public func stopAnimating() {
+        animateView.stopAnimating()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("LoadingView", owner: self, options: nil)
+        addSubview(containerView)
+        containerView.frame = self.bounds
+        containerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        containerView.addBlurAreaForLoading(area: containerView.bounds, style: .dark)
+        containerView.bringSubview(toFront: messageLabel)
+    }
+}
+
+extension UIView {
+    func addBlurAreaForLoading(area: CGRect, style: UIBlurEffectStyle) {
+        let effect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: effect)
+        let container = UIView(frame: area)
+        blurView.frame = CGRect(x: 0, y: 0, width: area.width, height: area.height)
+        container.addSubview(blurView)
+        self.insertSubview(container, at: 1)
     }
 }
--- RxSwift+MVVM/SupportFiles/LoadingView/LoadingViewable.swift
+++ RxSwift+MVVM/SupportFiles/LoadingView/LoadingViewable.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 Mustafa GUNES. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol loadingViewable {
+    func startAnimating()
+    func stopAnimating()
+}
+
+extension loadingViewable where Self: UIViewController {
+    
+    func startAnimating() {
+        let animateLoading = LoadingView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        view.addSubview(animateLoading)
+        view.bringSubview(toFront: animateLoading)
+        
+        animateLoading.restorationIdentifier = "loadingView"
+        animateLoading.center = view.center
+        animateLoading.loadingViewMessage = "Loading"
+        animateLoading.cornerRadius = 15
+        animateLoading.clipsToBounds = true
+        animateLoading.startAnimating()
+    }
+    
+    func stopAnimating() {
+        for item in view.subviews where item.restorationIdentifier == "loadingView" {
+            UIView.animate(withDuration: 0.3, animations: {
+                item.alpha = 0
+            }) { (_) in
+                item.removeFromSuperview()
+            }
+        }
+    }
+}
